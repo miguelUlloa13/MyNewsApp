e6793930ea25cb9bee20ee63b9809f6096ae56e2
Miguel Ulloa <prueba@correo.com>
2024-02-02 10:21:25 -0600

    Se modifico la interfaz Home Aagrego la table view

---
+++ MyNewsApp/Controller/NewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NewTableViewCell.swift
+//  MyNewsApp
+//
+//  Created by Miguel Ulloa on 02/02/24.
+//
+
+import UIKit
+
+class NewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
